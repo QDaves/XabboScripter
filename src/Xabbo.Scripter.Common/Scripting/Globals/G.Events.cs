e33e35dd23ac7bb76edf7c763c6f50ed190a004a
QDave <davidav08@gmail.com>
2026-02-10 20:07:48 +0100

    fix event

--- src/Xabbo.Scripter.Common/Scripting/Globals/G.Events.cs
+++ src/Xabbo.Scripter.Common/Scripting/Globals/G.Events.cs
@@ -158,6 +158,10 @@ public partial class G
     /// </summary>
     public void OnInventoryItemAdded(Action<InventoryItemEventArgs> callback) => Register(_inventoryManager, nameof(_inventoryManager.ItemAdded), callback);
 
+    public void OnInventoryItemUpdated(Action<InventoryItemEventArgs> callback) => Register(_inventoryManager, nameof(_inventoryManager.ItemUpdated), callback);
+
+    public void OnInventoryItemRemoved(Action<InventoryItemEventArgs> callback) => Register(_inventoryManager, nameof(_inventoryManager.ItemRemoved), callback);
+
     /// <summary>
     /// Registers a callback that is invoked when a floor item is updated.
     /// This happens when the floor item is moved or rotated.
